b19c64c4a72d1eb7d6f2dba0f4549a316de36e4e
Aleksa <lukicaleksa04@gmail.com>
2020-10-13 18:51:29 +0200

    Fix get service for AR

--- src/service/AdvertisingRequestService.ts
+++ src/service/AdvertisingRequestService.ts
@@ -11,8 +11,9 @@ export class AdvertisingRequestService extends AbstractService<AdvertisingReques
     async getAll(): Promise<AdvertisingRequest[]> {
         return await AdvertisingRequest.find({
             relations: [
-                'id_location', 'id_estate_sub_category',
-                'id_transaction_type', 'id_user_info'
+                'id_location', 'id_estate_sub_category','id_estate_sub_category.id_estate_category',
+                'id_transaction_type', 'id_user_info',
+                'id_location.id_part_of_city','id_location.id_part_of_city.id_city'
             ]
         })
     }
